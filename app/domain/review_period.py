b7235d6bd4884392f09717e0b12d42945ad3dcff
Kirill K <kovalev.kirill.a@gmail.com>
2026-01-08 21:26:14 +0300

    refactor: rework ReviewPeriod objects

--- app/domain/review_period.py
+++ app/domain/review_period.py
@@ -1,4 +1,7 @@
 import datetime
+from typing import Any, override
+
+from pydantic import BaseModel
 
 from app.domain.errors import DomainError
 
@@ -7,46 +10,42 @@ class ErrorReviewPeriodStartAfterEnd(DomainError):
     """ReviewPeriod start date must be after end date."""
 
 
-class ReviewPeriod:
-    def __init__(
-        self,
-        start: datetime.date,
-        end: datetime.date,
-    ):
-        self.start = start
-        self.end = end
+class ReviewPeriod(BaseModel):
+    """Basic review period."""
 
-    def create(self):
-        if not self.start >= self.end:
+    end: datetime.date
+
+    # May be calculated later
+    start: datetime.date | None = None
+
+    length: datetime.timedelta = datetime.timedelta(days=7)
+
+    @override
+    def model_post_init(self, context: Any) -> None:
+        if self.start is not None and self.start >= self.end:
             raise ErrorReviewPeriodStartAfterEnd
 
-        # TODO: Gather facts to review
-
-        # self.add_event(SomeEvent(param=self.param))
+        self.start = self.end - self.length
 
 
 class ReviewPeriodWeek(ReviewPeriod):
-    def __init__(self, end: datetime.date):
-        self.end = end
-        self.start = self.end - datetime.timedelta(days=7)
+    """One week."""
+
+    length: datetime.timedelta = datetime.timedelta(days=7)
 
 
 class ReviewPeriodMonth(ReviewPeriod):
-    def __init__(self, end: datetime.date):
-        self.end = end
-        self.start = self.end - datetime.timedelta(days=28)
+    """One month."""
 
-        # TODO: Month should be real month, calculate dates as "from 1 to 28/30/31 day"
+    # TODO: Month should be real month, calculate dates as "from 1 to 28/30/31 day"
+
+    length: datetime.timedelta = datetime.timedelta(days=28)
 
 
 # TODO: Implement Quarter with month-bound dates
 # class ReviewPeriodQuarter(ReviewPeriod):
-#     def __init__(self, end: datetime.date):
-#         self.end = end
-#         self.start = self.end - datetime.timedelta(days=90)
+#     length: datetime.timedelta = datetime.timedelta(days=90)
 
 # TODO: Implement Year with proper dates 01-01 to 31-12
 # class ReviewPeriodYear(ReviewPeriod):
-#     def __init__(self, end: datetime.date):
-#         self.end = end
-#         self.start = self.end - datetime.timedelta(days=365)
+#     length: datetime.timedelta = datetime.timedelta(days=365)
